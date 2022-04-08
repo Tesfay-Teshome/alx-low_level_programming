a974274d60c68c0f7dd7403eebc99f7bdf8bb7f9
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 10:58:13 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,20 +5,16 @@
  * Return: Always 0 (Success/correct)
  */
 int main(void)
-
 {
 char c;
-
 for (c = 'a'; c <= 'z'; c++)
 {
 putchar(c);
 }
-
 for (c = 'A'; c <= 'Z'; c++)
 {
 putchar(c);
 }
-
 putchar('\n');
 return (0);
 }
