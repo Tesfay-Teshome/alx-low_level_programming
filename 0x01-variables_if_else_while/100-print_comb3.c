8c967d31163d457bfe863d82943a90323654bd0c
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 13:01:31 -0700

    Task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int digit1, digit2;
 
 for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (digit2n = digit1 + 1; digit2 < 10; digit2++)
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
@@ -20,7 +20,8 @@ continue;
 
 putchar(',');
 putchar(' ');
-
+}
+}
 return (0);
 
 }
