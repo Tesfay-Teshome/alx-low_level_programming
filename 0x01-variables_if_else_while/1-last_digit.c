1f10b50a17b497fda140ec83982b7f69d05e1372
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 08:49:30 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ printf("Last digit of %d is %d and is 0\n", n, lastd);
 }
 else if (lastd < 6 && lastd != 0)
 {
-pintf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 }
 
 return (0);
