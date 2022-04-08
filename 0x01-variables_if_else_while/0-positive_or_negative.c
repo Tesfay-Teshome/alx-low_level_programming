5a1606eef4f698dd8004d86e52efbc8bccb60f7c
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 08:20:31 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,19 +9,18 @@
  *     */
 int main(void)
 {
-		int n;
+      int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
+      srand(time(0));
+      n = rand() - RAND_MAX / 2;
 
-					if (n > 0)
-							printf("%d is positive\n", n);
-						else if (n == 0)
-								printf("%d is zero\n", n);
-							else
-									printf("%d is negative\n", n);
-
-								return (0);
+      if (n > 0)
+      printf("%d is positive\n", n);
+      else if (n == 0)
+      printf("%d is zero\n", n);
+      else
+      printf("%d is negative\n", n);
+      return (0);
 
 }
 
