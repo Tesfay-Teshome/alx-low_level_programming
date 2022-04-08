a29e6c764a2f82697b20f84287dc5e8e1680af66
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 12:04:47 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+
+}
