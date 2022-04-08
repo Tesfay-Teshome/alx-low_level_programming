1d4864739ba1e7569a5920d1466aeef78dda6426
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 14:08:40 -0700

    Task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * combination of three numbers
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+int dgt1, dgt2, dgt3;
+
+for (dgt1 = 0; dgt1 < 8; dgt1++)
+{
+for (dgt2 = dgt1 + 1; dgt < 9; dgt2++)
+{
+for (dgt3 = dgt2 + 1; dgt < 10; dgt3++)
+{
+putchar ((dgt1 % 10) + '0');
+putchar ((dgt2 % 10) + '0');
+putchar ((dgt3 % 10) + '0');
+if (dgt1 == 7 && dgt2 == 8 && dgt3 == 9)
+continue;
+putchar (',')
+putchar (' ')
+}
+}
+}
+putchar ('\n');
+
+return(0);
+
+}
