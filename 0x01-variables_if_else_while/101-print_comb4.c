df1e860f5acf05cf4e5a94faafd749433ac359d3
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 14:12:42 -0700

    Task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ for (dgt1 = 0; dgt1 < 8; dgt1++)
 {
 for (dgt2 = dgt1 + 1; dgt2 < 9; dgt2++)
 {
-for (dgt3 = dgt2 + 1; dgt < 10; dgt3++)
+for (dgt3 = dgt2 + 1; dgt3 < 10; dgt3++)
 {
 putchar ((dgt1 % 10) + '0');
 putchar ((dgt2 % 10) + '0');
