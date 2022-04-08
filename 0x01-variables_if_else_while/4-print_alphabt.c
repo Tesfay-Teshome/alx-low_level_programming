05ff90fd798f9d76263e2f0242ab49866bccd629
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 11:34:13 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+}
+
+putchar('\n');
+
+return (0);
+
+}
