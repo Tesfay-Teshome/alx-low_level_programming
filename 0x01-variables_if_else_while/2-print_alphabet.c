f95ec07dda73458f1069292e4f79895aaf71d306
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 09:05:39 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+nclude <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+
+putchar('\n');
+return (0);
+
+}
