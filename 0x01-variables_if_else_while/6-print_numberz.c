c0af8220c0d3f0e6e18e593587098557c90b5888
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-08 11:45:04 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+
+putchar('\n');
+
+return (0);
+
+}
