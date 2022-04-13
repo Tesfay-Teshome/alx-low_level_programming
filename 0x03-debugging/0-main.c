fa0cd0f353c7d6112111ef5d403a2b0b15ea4a4d
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-13 02:27:01 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests function prints if integer postive or negative
- * Return: 0
- */
+* main - tests function prints if integer positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 
 	 i = 0;
-	postive_or_negative(i);
+	positive_or_negative(i);
 
 	return (0);
 }
