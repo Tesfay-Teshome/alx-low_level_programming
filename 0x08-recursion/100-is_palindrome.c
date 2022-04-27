cb364283a9434dd82a8f07fc55228959c2355c84
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-27 01:21:26 -0700

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,11 +8,11 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int palind2(char *a, int l)
+int p2(char *a, int l)
 {
 	if (*a == 0)
 		return (l - 1);
-	return (palind2(a + 1, l + 1));
+	return (p2(a + 1, l + 1));
 }
 /**
  * palind3 - compares string vs string reverse
@@ -23,13 +23,13 @@ int palind2(char *a, int l)
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int palind3(char *a, int l)
+int p3(char *a, int l)
 {
 	if (*a != *(a + l))
 		return (0);
 	else if (*a == 0)
 		return (1);
-	return (palind3(a + 1, l - 2));
+	return (p3(a + 1, l - 2));
 }
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -42,6 +42,6 @@ int is_palindrome(char *s)
 {
 	int l;
 
-	l = palind2(s, 0);
-	return (palind3(s, l));
+	l = p2(s, 0);
+	return (p3(s, l));
 }
