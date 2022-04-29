1e9a5c3801d654259a5d36094d6fc5d7817e375e
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-29 06:16:57 -0700

    Main Header

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,5 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -18,3 +19,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
