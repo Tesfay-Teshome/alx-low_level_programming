2c83e193221d70a682f1cb1fbf5d98f9825dc70f
Tesfay-Teshome <tesfix2017@gmail.com>
2022-04-29 09:18:57 -0700

    Task 0

---
+++ 0x09-static_libraries/four.c
@@ -0,0 +1,13 @@
+#include "main.h"
+char* _strncpy(char* dest, char* src, int n) {
+	return 0;
+}
+int _strcmp(char* s1, char* s2) {
+	return 0;
+}
+char* _memset(char* s, char b, unsigned int n) {
+	return 0;
+}
+char* _memcpy(char* dest, char* src, unsigned int n) {
+	return 0;
+}
