e88624edc0cce5c866d63244835c56e6498dc572
Tesfay-Teshome <tesfix2017@gmail.com>
2022-05-12 05:23:59 -0700

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,6 +8,7 @@
  * Return: no return.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
+
 {
 
 va_list valist;
@@ -23,11 +24,10 @@ str = va_arg(valist, char *);
 if (str)
 printf("%s", str);
 else
-
 printf("(nil)");
 
 if (i < n - 1)
- if (separator)
+if (separator)
 printf("%s", separator);
 }
 
