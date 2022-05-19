fad9f122f683ad36c077d41fbaef5f5f8c2b5d2f
Tesfay-Teshome <tesfix2017@gmail.com>
2022-05-19 06:11:47 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,10 +11,10 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		printf("[%d] %s\n", 0, "(nill");
+		printf("[%d] %s\n", 0, "(nil)");
 		else
 		printf("[%d] %s\n", h->len, h->str);
-		h = h-next;
+		h = h->next;
 		tes++;
 	}
 	return(tes);
