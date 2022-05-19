dc9acd2bd1aac771012737967ede66f3623ef089
Tesfay-Teshome <tesfix2017@gmail.com>
2022-05-19 06:22:41 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_list - prints all the elements  a list_t list.
  * @h: singly linked list.
@@ -6,8 +7,8 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t nelem;
-nelem = 0;
+size_t tes;
+tes = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
@@ -15,7 +16,7 @@ printf("[%d] %s\n", 0, "(nil)");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
-nelem++;
+tes++;
 }
-return (nelem);
+return (tes);
 }
