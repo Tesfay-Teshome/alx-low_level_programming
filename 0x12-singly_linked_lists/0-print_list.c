dcb5b7e5820100017e4ec3e6d83954d08de27131
Tesfay-Teshome <tesfix2017@gmail.com>
2022-05-19 06:27:31 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
