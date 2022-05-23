cdaee0760c83d6e15ed9eedfb60f7ca572d25146
Tesfay-Teshome <tesfix2017@gmail.com>
2022-05-23 05:05:40 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
+size_t num = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        num++;
-        h = h->next;
-    }
+while (h)
+{
+printf("%d\n", h->n);
+num++;
+h = h->next;
+}
 
-    return (num);
+return (num);
 }
