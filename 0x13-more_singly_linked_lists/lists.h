4dfdfdb22e95e893d042bcae51d0d351df8f3931
Tesfay-Teshome <tesfix2017@gmail.com>
2022-05-23 09:40:18 -0700

    List Header and Prototypes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,12 +10,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX Project
+ * for Holberton project
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
