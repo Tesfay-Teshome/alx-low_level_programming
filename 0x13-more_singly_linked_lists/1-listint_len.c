17cd117d0bd544511113f7cdefb7a953173229fb
Tesfay-Teshome <tesfix2017@gmail.com>
2022-05-23 03:43:59 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <lists.h>
+
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked lists of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return(0);
+}
