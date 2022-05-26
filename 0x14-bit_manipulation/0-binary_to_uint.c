eb62c5afb8e62bb284263782ff6d4cd19a04bede
Tesfay-Teshome <tesfix2017@gmail.com>
2022-05-26 06:13:32 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_val = 0;
+	unsigned int decimal_value = 0;
 
 	if (!b)
 		return (0);
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		decimal_value = 2 * decimal_value + (b[i] - '0');
 	}
-	return (dec_val);
+	return (decimal_value);
 }
