2c56d77ac464e849e745b498872b7260eb977652
Tesfay-Teshome <tesfix2017@gmail.com>
2022-07-11 10:41:33 -0700

    All C files

---
+++ 0x18-dynamic_libraries/libs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
+int sub(int a, int b)
+{
+int c;
+c = a - b;
+return (c);
+}
+int mul(int a, int b)
+{
+int c;
+c = a * b;
+return (c);
+}
+div_t div(int a, int b);
+
+int mod(int a, int b)
+{
+int c;
+c = a % b;
+return (c);
+}
+
