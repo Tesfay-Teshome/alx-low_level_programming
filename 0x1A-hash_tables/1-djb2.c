1697a176e31925965749dd3a3d260e6610957846
Tesfay-Teshome <tesfix2017@gmail.com>
2022-07-29 04:58:27 -0700

    Djb2 Algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -13,7 +13,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
-		hash = ((hash << 5) + hash) + c;
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-	return (hash)
+	return (hash);
 }
